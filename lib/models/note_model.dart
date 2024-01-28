36ab74aefa62e41ffd89d65d2ceafdbf903b6cf7
Usame <ussama14863@gmail.com>
2024-01-28 13:54:51 +0300

    generate type Adapter

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,9 +1,16 @@
 
+import 'package:hive/hive.dart';
 
+part 'note_model.g.dart';
+@HiveType(typeId: 0) // unique
 class NoteModel {
+  @HiveField(0)      // unique in this class
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteModel(
